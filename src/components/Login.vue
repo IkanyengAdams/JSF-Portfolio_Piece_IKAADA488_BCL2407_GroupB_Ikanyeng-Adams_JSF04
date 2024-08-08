cef797f82730bae243cf9864943be10b6e3d710a
Ikanyeng Adams <ikanyengadams2@gmail.com>
2024-08-08 11:12:04 +0200

    Added login form and functionality to Login.vue component

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,18 +1,41 @@
 <template>
-  <div>
-    <h1>Login page</h1>
-    <button @click="goToProductList">Back to Product List</button>
+  <div class="login-container">
+    <h1>Login Page</h1>
+    <form @submit.prevent="login">
+      <div class="form-group">
+        <label for="username">Username:</label>
+        <input type="text" v-model="username" id="username" required />
+      </div>
+      <div class="form-group">
+        <label for="password">Password:</label>
+        <input type="password" v-model="password" id="password" required />
+      </div>
+      <button type="submit" class="login-button">Login</button>
+    </form>
+    <button @click="goToProductList" class="back-button">Back to Product List</button>
   </div>
+
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      username: '',
+      password: '',
+    };
+  },
   methods: {
+    login() {
+      console.log('Login button clicked');
+
+    },
     goToProductList() {
       this.$router.push('/');
     }
   }
 };
+
 </script>
 
 <style scoped>

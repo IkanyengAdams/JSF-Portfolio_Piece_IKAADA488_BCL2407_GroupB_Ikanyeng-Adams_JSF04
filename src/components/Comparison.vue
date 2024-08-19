b3f0609e47959a447528b1ffe5ce024b8050f8df
Ikanyeng Adams <ikanyengadams2@gmail.com>
2024-08-19 11:36:39 +0200

    Added comparison table to Comparison.vue component, displaying item titles, images, and descriptions, and updated button styling

--- src/components/Comparison.vue
+++ src/components/Comparison.vue
@@ -2,11 +2,36 @@
   <div>
     <h1>Comparison page</h1>
     <button @click="goToProductList">Back to Product List</button>
+    <div v-if="comparisonItems.length > 0" class="comparison-table">
+      <table>
+        <thead>
+          <tr>
+            <th v-for="(item, index) in comparisonItems" :key="index">{{ item.title }}</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td v-for="(item, index) in comparisonItems" :key="index">
+              <img :src="item.image" :alt="item.title" class="comparison-image" />
+            </td>
+          </tr>
+          <tr>
+            <td v-for="(item, index) in comparisonItems" :key="index">{{ item.description }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <p v-else>No items to compare</p>
   </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      comparisonItems: JSON.parse(localStorage.getItem('comparison')) || []
+    };
+  },
   methods: {
     goToProductList() {
       this.$router.push('/');
@@ -16,7 +41,7 @@ export default {
 </script>
 
 <style scoped>
-button{
+button {
   background-color: #4c61af;
 }
 </style>
